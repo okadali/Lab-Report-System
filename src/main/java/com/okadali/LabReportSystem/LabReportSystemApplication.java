f119476677ebbf7285fa2cde927ff8a0aff0d14f
okadali <okadali@hotmail.com>
2023-03-08 12:13:26 +0300

    Initial commit

---
+++ src/main/java/com/okadali/LabReportSystem/LabReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.okadali.LabReportSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabReportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okadali/LabReportSystem/LabReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okadali.LabReportSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
